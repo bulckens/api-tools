0e7a73f50f029f5e53673a469cfebc42e94c60fe
wout <wout@impinc.co.uk>
2017-01-12 16:42:34 +0100

    Initialising request with source

--- src/Bulckens/ApiTools/V1/Api.php
+++ src/Bulckens/ApiTools/V1/Api.php
@@ -5,8 +5,8 @@ namespace Bulckens\ApiTools\V1;
 class Api {
 
   // Build request
-  public static function request() {
-    return new Request();
+  public static function request( $source ) {
+    return new Request( $source );
   }
 
 }
--- src/Bulckens/ApiTools/V1/Request.php
+++ src/Bulckens/ApiTools/V1/Request.php
@@ -5,8 +5,9 @@ namespace Bulckens\ApiTools\V1;
 class Request extends Model {
 
   // Initialize new instance
-  public function __construct() {
-    $this->uri = '/api/v1';
+  public function __construct( $source ) {
+    $this->uri    = '/api/v1';
+    $this->source = $source;
   }
 
 }
