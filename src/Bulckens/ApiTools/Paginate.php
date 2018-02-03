e03d3d3360dd9e52cc46d4845e6bbe089497a208
wout <wout@mick-wout.com>
2018-02-03 18:27:43 +0100

    Removing magic get method in Paginate

--- src/Bulckens/ApiTools/Paginate.php
+++ src/Bulckens/ApiTools/Paginate.php
@@ -6,47 +6,51 @@ use Exception;
 
 class Paginate {
 
-  protected $value = [
-    'page' => 1
-  , 'per_page' => 15
-  ];
+  public $total;
+  public $pages;
+  public $range;
+  public $next;
+  public $previous;
+  public $necessary;
+  public $page = 1;
+  public $per_page = 15;
 
   public function __construct( $items, $options = [] ) {
     // store items
     if ( is_a( $items, 'Illuminate\Database\Eloquent\Collection' )) {
-      $this->value['total'] = $items->count();
+      $this->total = $items->count();
     } elseif ( is_array( $items )) {
-      $this->value['total'] = count( $items );
+      $this->total = count( $items );
     } elseif ( is_int( $items )) {
-      $this->value['total'] = $items;
+      $this->total = $items;
     } else {
-      throw new PaginateItemsNotAcceptableException( 'Expected Eloquent Collection, array or integer but got ' . gettype( $items ) );
+      throw new PaginateItemsNotAcceptableException( 'Expected Eloquent Collection, array or integer but got ' . gettype( $items ));
     }
 
     // store options
     if ( is_numeric( $options )) {
-      $this->value['per_page'] = $options;
+      $this->per_page = $options;
     } else {
       if ( isset( $options['page'] )) {
-        $this->value['page'] = $options['page'];
+        $this->page = $options['page'];
       }
 
       if ( isset( $options['per_page'] )) {
-        $this->value['per_page'] = $options['per_page'];
+        $this->per_page = $options['per_page'];
       }
     }
 
     // store total pages
-    if ( empty( $this->value['total'] ) ) {
-      $this->value['pages'] = 0;
+    if ( empty( $this->total ) ) {
+      $this->pages = 0;
     } else {
-      $this->value['pages'] = intval( ceil( $this->value['total'] / $this->value['per_page'] ) );
+      $this->pages = intval( ceil( $this->total / $this->per_page ));
 
       // store range
-      $start = ( $this->value['page'] - 1 ) * $this->value['per_page'] + 1;
-      $end = $this->value['page'] * $this->value['per_page'];
+      $start = ( $this->page - 1 ) * $this->per_page + 1;
+      $end = $this->page * $this->per_page;
 
-      $this->value['range'] = [
+      $this->range = [
         $start
       , $end
       , 'start' => $start
@@ -55,31 +59,32 @@ class Paginate {
     }
 
     // store next
-    if ( $this->value['page'] < $this->value['pages'] ) {
-      $this->value['next'] = $this->value['page'] + 1;
+    if ( $this->page < $this->pages ) {
+      $this->next = $this->page + 1;
     }
 
     // store previous
-    if ( $this->value['page'] > 1 ) {
-      $this->value['previous'] = $this->value['page'] - 1;
+    if ( $this->page > 1 ) {
+      $this->previous = $this->page - 1;
     }
 
     // store necessary
-    $this->value['necessary'] = $this->value['pages'] > 1;
+    $this->necessary = $this->pages > 1;
   }
 
 
   // Convert to array
   public function toArray() {
-    return $this->value;
-  }
-
-
-  // Get property
-  public function __get( $name ) {
-    if ( isset( $this->value[$name] )) {
-      return $this->value[$name];
-    }
+    return [
+     'total' => $this->total
+    , 'pages' => $this->pages
+    , 'range' => $this->range
+    , 'next' => $this->next
+    , 'previous' => $this->previous
+    , 'necessary' => $this->necessary
+    , 'page' => $this->page
+    , 'per_page' => $this->per_page
+    ];
   }
 
 }
